e498373848919cab4026f625ef2d5b9ccf22eb8d
Silas Maughan <silas.maughan@gmail.com>
2024-01-09 20:07:46 +0100

    sphere intersection

--- ray.h
+++ ray.h
@@ -8,7 +8,7 @@ class ray
 public:
     ray() {}
 
-    ray(const point3 origin, const vec3 direction) : orig(origin), dir(direction) {}
+    ray(const point3 &origin, const vec3 &direction) : orig(origin), dir(direction) {}
 
     point3 origin() const { return orig; }
     vec3 direction() const { return dir; }
