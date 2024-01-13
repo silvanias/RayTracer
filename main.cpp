56a5c2574d39565bd30c6e5fecd88dac705ccb08
Silas Maughan <silas.maughan@gmail.com>
2024-01-13 14:50:19 +0100

    Simplify hit_sphere func

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@
 double hit_sphere(const point3 &center, double radius, const ray &r)
 {
     vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = half_b*half_b - a*c;
+
 
     if (discriminant < 0)
     {
@@ -18,7 +19,7 @@ double hit_sphere(const point3 &center, double radius, const ray &r)
     }
     else
     {
-        return (-b - sqrt(discriminant)) / (2.0 * a);
+        return (-half_b - sqrt(discriminant)) / a;
     }
 }
 
